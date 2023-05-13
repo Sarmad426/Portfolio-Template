e9ae60169e6603e4d539800effe519b578fa274c
Sarmad rafique <sarmadrafique040@gmail.com>
2023-05-13 15:15:37 +0500

    Data file for protofolio

---
+++ components/data.tsx
@@ -0,0 +1,39 @@
+import sarmadImage from './my image.jpeg'
+export const personalData = {
+  name: "Muhammad Sarmad",
+  fatherName: "Muhammad Rafique",
+  age: 18,
+  image:sarmadImage,
+  description:
+    "My name is Muhammad Sarmad Rafique. I am undergraduate Software Engineer. I am a Full Stack developer with Next.js and postgresql. I can effectively communicate and can work full time.I've also worked with various technologies and i am a quick learner.",
+};
+export const skills = [
+  "HTML",
+  "CSS",
+  "Javascript/Typescript",
+  "Bootstrap",
+  "Tailwind Css",
+  "React",
+  "Next js",
+  'Python',
+  'Django',
+  'SQL',
+  'Git',
+  'Figma',
+];
+export const contact = {
+    phone:'+92 3261896636',
+    email:'sarmadrafique040@gmail.com',
+}
+export const protfolioLinks = {
+  github: "https://github.com/sarmad426",
+  facebook: "https://www.facebook.com/profile.php?id=100083350142289",
+  linkedin: "https://www.linkedin.com/in/sarmad-rafique-62518a22b",
+  twitter: "https://twitter.com/SarmadRafique2",
+  ecommerceApp: "https://progressivesite.netlify.app",
+};
+export const education = {
+    matric:'From faisalabad (2019)',
+    intermediate:'Intermediate from Nankana Sahib (2021)',
+    bachelors:'Undergraduate Software Engineer from University of South Asia',
+}
