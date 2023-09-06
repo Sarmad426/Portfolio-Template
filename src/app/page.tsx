7a19d5982c9fdd9b8dfea8370181ad1769eae5cb
Sarmad rafique <sarmadrafique040@gmail.com>
2023-09-06 08:32:49 +0500

    Formatted the code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import Protfolio from "../../components/protfolio"
+import Protfolio from "../../components/protfolio";
 
 export const metadata = {
-  title:'Sarmad Rafique Protofolio'
-}
+  title: "Sarmad Rafique Protofolio",
+};
 export default function Home() {
   return (
     <main className="bg-black text-white">
       <Protfolio />
     </main>
-  )
+  );
 }
