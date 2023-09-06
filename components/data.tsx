eddcdb4995ec770e156785301ec85127df840f70
Sarmad rafique <sarmadrafique040@gmail.com>
2023-09-06 08:32:38 +0500

    updated phone number

--- components/data.tsx
+++ components/data.tsx
@@ -1,9 +1,9 @@
-import sarmadImage from './my image.jpeg'
+import sarmadImage from "./my image.jpeg";
 export const personalData = {
   name: "Muhammad Sarmad",
   fatherName: "Muhammad Rafique",
   age: 18,
-  image:sarmadImage,
+  image: sarmadImage,
   description:
     "My name is Muhammad Sarmad Rafique. I am undergraduate Software Engineer. I am a Full Stack developer with Next.js and postgresql. I can effectively communicate and can work full time.I've also worked with various technologies and i am a quick learner.",
 };
@@ -15,16 +15,16 @@ export const skills = [
   "Tailwind Css",
   "React",
   "Next js",
-  'Python',
-  'Django',
-  'SQL',
-  'Git',
-  'Figma',
+  "Python",
+  "Django",
+  "SQL",
+  "Git",
+  "Figma",
 ];
 export const contact = {
-    phone:'+92 3261896636',
-    email:'sarmadrafique040@gmail.com',
-}
+  phone: "+92 3001234566",
+  email: "sarmadrafique040@gmail.com",
+};
 export const protfolioLinks = {
   github: "https://github.com/sarmad426",
   facebook: "https://www.facebook.com/profile.php?id=100083350142289",
@@ -33,7 +33,7 @@ export const protfolioLinks = {
   ecommerceApp: "https://progressivesite.netlify.app",
 };
 export const education = {
-    matric:'From faisalabad (2019)',
-    intermediate:'Intermediate from Nankana Sahib (2021)',
-    bachelors:'Undergraduate Software Engineer from University of South Asia',
-}
+  matric: "From faisalabad (2019)",
+  intermediate: "Intermediate from Nankana Sahib (2021)",
+  bachelors: "Undergraduate Software Engineer from University of South Asia",
+};
