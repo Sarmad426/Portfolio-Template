8b6412456683f37d87c221b2435a6f26d1f66c2d
Sarmad rafique <sarmadrafique040@gmail.com>
2023-09-22 12:03:15 +0500

    Navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+const Navbar = () => {
+  const links = [
+    {
+      label: "Projects",
+      url: "#projects",
+    },
+    {
+      label: "Skills",
+      url: "#skills",
+    },
+    {
+      label: "Contact",
+      url: "#contact",
+    },
+    {
+      label: "Social Links",
+      url: "#links",
+    },
+    {
+      label: "Education",
+      url: "#education",
+    },
+  ];
+  return (
+    <nav className="lg:flex lg:w-1/2 lg:mx-auto md:flex hidden items-center justify-center space-x-16 bg-neutral-200 py-3 text-lg font-bold pt-6">
+      {links.map((link) => {
+        return (
+          <a
+            key={link.label}
+            href={link.url}
+            className={`${inter.className} hover:underline`}
+          >
+            {link.label}
+          </a>
+        );
+      })}
+    </nav>
+  );
+};
+
+export default Navbar;
