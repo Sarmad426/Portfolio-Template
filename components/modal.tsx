9ed7c9f34373b969f171827c55da1a04330ca0f5
Sarmad rafique <sarmadrafique040@gmail.com>
2023-09-23 10:56:44 +0500

    Added pages and features data on modals

--- components/modal.tsx
+++ components/modal.tsx
@@ -9,7 +9,7 @@ interface Project {
 }
 
 const Modal: React.FC<Project> = ({ projectData, id }) => {
-  const { name, url, description } = projectData;
+  const { name, url, description, pages, features } = projectData;
   const closeModal = () => {
     const dialog = document.getElementById(`${id}`);
     if (dialog) {
@@ -20,10 +20,10 @@ const Modal: React.FC<Project> = ({ projectData, id }) => {
   return (
     <dialog
       id={`${id}`}
-      className="px-6 py-8 border-0 outline-0 rounded-md lg:w-1/3 md:w-2/3"
+      className="border-0 outline-0 rounded-md lg:w-2/5 md:w-2/3 sm:w-5/6 w-full"
     >
-      <div className="relative">
-        <button className="absolute right-3 top-0 outline-none shadow-md hover:shadow-sm rounded-full px-3 py-2 transition-colors duration-500">
+      <div className="px-6 py-8 relative">
+        <button className="absolute right-3 top-0 outline-none shadow-md hover:shadow-sm rounded-full p-2 transition-colors duration-500">
           <IoMdClose
             className="w-6 h-6 shrink-0 text-end"
             onClick={closeModal}
@@ -31,6 +31,34 @@ const Modal: React.FC<Project> = ({ projectData, id }) => {
         </button>
         <h1 className="my-3 text-2xl font-semibold">{name}</h1>
         <p className="my-4 text-base text-gray-600">{description}</p>
+        <div className="flex items-center justify-between space-x-4">
+          <ul className="my-3">
+            <h2 className="text-xl font-medium">Pages</h2>
+            {pages.map((page) => {
+              return (
+                <li
+                  key={page}
+                  className="text-xs font-light text-gray-600 list-decimal font-[sans-serif]"
+                >
+                  {page}
+                </li>
+              );
+            })}
+          </ul>
+          <ul className="my-3">
+            <h2 className="text-xl font-medium">Features</h2>
+            {features.map((feature) => {
+              return (
+                <li
+                  key={feature}
+                  className="text-sm font-light text-gray-700 font-[calibri]"
+                >
+                  {feature}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
         <a
           href={url}
           className="text-blue-600 underline text-base"
