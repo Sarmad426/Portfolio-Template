66509cda58e9520ba978ea534d236e7153eefb3e
Sarmad rafique <sarmadrafique040@gmail.com>
2024-08-13 22:40:27 +0500

    Added color variables for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primaryColor: "var(--primary-color)",
+        secondaryColor: "var(--secondary-color)",
+        darkPrimaryColor: "var(--dark-primary-color)",
+        darkSecondaryColor: "var(--dark-secondary-text-color)",
       },
     },
   },
   plugins: [],
-}
+};
